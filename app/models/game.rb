f697169f7ca76afac998c49c346f52bbc534ac5b
balzhunter <balvirp1@gmail.com>
2021-08-04 23:01:38 +0000

    Game validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,9 @@
 class Game < ApplicationRecord
+  validates :name, :genre, :price, :release_date, presence: true
+  validates :genre,
+            inclusion: {
+              in: %w(Simulator Adventure Strategy Role-playing\ (RPG) Shooter Fighting
+                     Sport)
+            }
+  validates :price, numericality: { only_integer: true, greater_than: 0 }
 end
